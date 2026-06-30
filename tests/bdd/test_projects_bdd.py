01cc712ef001bd30c68e673d82c79fe7d46a7ac5
Lovegiver <frederic.courcier@gmail.com>
2026-06-30 22:26:39 +0200

    feat(bdd): use shared step modules for projects

--- tests/bdd/test_projects_bdd.py
+++ tests/bdd/test_projects_bdd.py
@@ -1,7 +1,6 @@
 from pytest_bdd import scenarios
 
 from tests.bdd.steps.authentication_steps import *  # noqa: F403,F401
-from tests.bdd.steps.project_steps import *  # noqa: F403,F401
 from tests.bdd.steps.response_steps import *  # noqa: F403,F401
 from tests.bdd.steps.user_steps import *  # noqa: F403,F401
 
