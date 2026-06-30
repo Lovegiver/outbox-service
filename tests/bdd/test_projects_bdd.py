01b5f517d28bc748dfe7e15049ce5c80557ee3c0
Lovegiver <frederic.courcier@gmail.com>
2026-06-30 22:24:49 +0200

    feat(bdd): wire projects step modules

--- tests/bdd/test_projects_bdd.py
+++ tests/bdd/test_projects_bdd.py
@@ -1,4 +1,9 @@
 from pytest_bdd import scenarios
 
+from tests.bdd.steps.authentication_steps import *  # noqa: F403,F401
+from tests.bdd.steps.project_steps import *  # noqa: F403,F401
+from tests.bdd.steps.response_steps import *  # noqa: F403,F401
+from tests.bdd.steps.user_steps import *  # noqa: F403,F401
+
 
 scenarios("features/projects.feature")
